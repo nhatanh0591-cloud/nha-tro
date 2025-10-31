c7f6d3cd12419952c7ae66c6db3ab6df5225d2b1
Dang Nhat Anh <nhatanh@nha-tro.com>
2025-10-31 16:06:01 +0700

    Complete PWA setup with install prompt

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,45 @@
-const CACHE_NAME = 'nha-tro-v1';
+const CACHE_NAME = 'nha-tro-v2';
+const urlsToCache = [
+  '/app-customer.html',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
+// Install
 self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
+// Activate
 self.addEventListener('activate', event => {
-  console.log('Service Worker activated.');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
+// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
